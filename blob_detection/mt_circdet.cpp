2af1e5a5f7e6322fe192bbe0aa99eba16ae73722
Francesco Ferrara <fr.ferrara@gmail.com>
2011-12-18 20:51:04 +0100

    Added library for the multi threaded circle detection

---
+++ blob_detection/mt_circdet.cpp
@@ -0,0 +1,56 @@
+/*
+ * mt_circdet.cpp
+ *
+ *  Created on: Dec 18, 2011
+ *      Author: ferraraf
+ */
+
+
+#include "mt_circdet.hpp"
+
+
+namespace mt_circdet
+{
+// Multi-threading callback function
+void CircleDetector::operator()( const blocked_range< size_t > & r ) const
+{
+	// Fill the histograms
+	for ( size_t i = r.begin(); i != r.end(); i++ )
+	{
+		// Matrix for the randomly selected blob points
+		MatrixXd x;
+		x.resize( num_points, 2 );
+
+		// Vector for the random indeces
+		vector< double > rand_num = multi_uniGen( num_points, 0, ( double )blob_contour.size() - 1.0 );
+
+		// Get the randomly selected blob points
+		for ( int j = 0; j < num_points; j++ )
+		{
+			x( j, 0 ) = ( double )blob_contour[ ( int )round( rand_num[ j ] ) ][ 0 ];
+			x( j, 1 ) = ( double )blob_contour[ ( int )round( rand_num[ j ] ) ][ 1 ];
+		}
+
+		// Fit a circle
+		Vector2d x_c;
+		double r;
+		circdet::detect_circle( x, x_c, r );
+
+		// Increment histograms
+		gsl_histogram_increment( hist_r, round( r ) );
+		gsl_histogram2d_increment( hist__x_c, round( x_c( 0 ) ), round( x_c( 1 ) ) );
+	}
+}
+
+// Get the center and radius of the circle
+void get_circle( gsl_histogram * hist_r, \
+				 gsl_histogram2d * hist__x_c, \
+				 size_t & x_c, \
+				 size_t & y_c, \
+				 size_t & r )
+{
+	// Get the maximum histogram values
+	r = gsl_histogram_max_bin( hist_r );
+	gsl_histogram2d_max_bin( hist__x_c, &x_c, &y_c );
+}
+}
---
+++ blob_detection/mt_circdet.hpp
@@ -0,0 +1,56 @@
+/*
+ * mt_circdet.hpp
+ *
+ *  Created on: Dec 18, 2011
+ *      Author: ferraraf
+ */
+
+#ifndef MT_CIRCDET_HPP_
+#define MT_CIRCDET_HPP_
+
+
+// Histograms
+#include <gsl/gsl_histogram.h>
+#include <gsl/gsl_histogram2d.h>
+
+// Intel threading libs
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+// Own libs
+#include <rand_gen.hpp>
+#include <circdet.hpp>
+
+
+using namespace tbb;
+
+
+namespace mt_circdet
+{
+class CircleDetector
+{
+	// Number of points
+	unsigned int num_points;
+
+	// Blob contour
+	vector< vector< unsigned int > > blob_contour;
+
+	// Histograms for the circle detection
+	gsl_histogram * hist_r;
+	gsl_histogram2d * hist__x_c;
+
+public:
+	// Multi-threading callback function
+	void operator()( const blocked_range< size_t > & r ) const;
+};
+
+// Get the center and radius of the circle
+void get_circle( gsl_histogram * hist_r, \
+				 gsl_histogram2d * hist__x_c, \
+				 size_t & x_c, \
+				 size_t & y_c, \
+				 size_t & r );
+}
+
+
+#endif /* MT_CIRCDET_HPP_ */
